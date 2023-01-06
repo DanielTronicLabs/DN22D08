fbdd5d348229f5654486f8f1ba20c670d9624242
DanielTronicLabs <121263848+DanielTronicLabs@users.noreply.github.com>
2023-01-05 20:10:16 -0500

    Update DN22D08.cpp

--- DN22D08.cpp
+++ DN22D08.cpp
@@ -1,3 +1,16 @@
+/*
+||
+|| @file DN22D08.cpp
+|| @version 1.0
+|| @author DanielTronic Labs
+|| @contact danieltronic808@gmail.com
+||
+|| @description
+||   This library provides a simple interface to use 
+||   a PLC IO RS485 DN22D08 controls the output relays and 
+||   multiplexes the 7-segment display to show numerical values.
+*/
+
 #include "Arduino.h"
 #include "DN22D08.h"
  
